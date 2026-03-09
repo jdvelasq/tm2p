fd65f21e479ff127d816465aae08c991d2ffa29e
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-03-09 18:36:15 -0500

    Update apply_thesaurus.py

--- tm2p/refine/thesaurus_old/references/general/apply_thesaurus.py
+++ tm2p/refine/thesaurus_old/references/general/apply_thesaurus.py
@@ -37,14 +37,13 @@ import sys
 import pandas as pd
 
 from tm2p._intern import ParamsMixin
-from tm2p._intern.data_access import load_all_records_from_database, save_main_csv_zip
+from tm2p._intern.data_access import save_main_csv_zip
 
 
 #
 #
 class ApplyThesaurus(
     ParamsMixin,
-    ThesaurusMixin,
 ):
     """:meta private:"""
 
